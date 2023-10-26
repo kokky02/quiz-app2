177782cbb3d34b1e4076c8d297da20af60002241
Kokky02 <daniel.kokay@gmail.com>
2023-10-26 20:12:44 +0200

    created function for click on continue btn - move the page quiz-section

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const startBtn = document.querySelector('.start-btn')
 const popupInfo = document.querySelector('.popup-info')
 const exitBtn = document.querySelector('.exit-btn')
 const main = document.querySelector('.main')
+const continueBtn = document.querySelector('.continue-btn')
+const quizSection = document.querySelector('.quiz-section')
 
 startBtn.onclick = () => {
      popupInfo.classList.add('active')
@@ -9,6 +11,12 @@ startBtn.onclick = () => {
 }
 
 exitBtn.onclick = () => {
+     popupInfo.classList.remove('active')
+     main.classList.remove('active')
+}
+
+continueBtn.onclick = () => {
+     quizSection.classList.add('active')
      popupInfo.classList.remove('active')
      main.classList.remove('active')
 }
