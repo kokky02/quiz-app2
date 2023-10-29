7e222335bfbd8f08b56bedbc2fa7892fb8086fe3
Kokky02 <daniel.kokay@gmail.com>
2023-10-29 11:43:58 +0100

    created new const (quizBox) in script

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector('.exit-btn')
 const main = document.querySelector('.main')
 const continueBtn = document.querySelector('.continue-btn')
 const quizSection = document.querySelector('.quiz-section')
+const quizBox = document.querySelector('.quiz-box')
 
 startBtn.onclick = () => {
      popupInfo.classList.add('active')
