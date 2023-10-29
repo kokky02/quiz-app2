ec2e21a951677ea07e0421b3a96815179570a025
Kokky02 <daniel.kokay@gmail.com>
2023-10-29 13:05:04 +0100

    added optionTag in showQuestions funcion

--- script.js
+++ script.js
@@ -42,4 +42,11 @@ const optionList = document.querySelector('.option-list')
 function showQuestions(index){
      const questionText = document.querySelector('.question-text')
      questionText.textContent = `${questions[index].numb}. ${questions[index].question}`
+
+     let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+                    <div class="option"><span>${questions[index].options[1]}</span></div>
+                    <div class="option"><span>${questions[index].options[2]}</span></div>
+                    <div class="option"><span>${questions[index].options[3]}</span></div>`
+
+     optionList.innerHTML = optionTag
 }
